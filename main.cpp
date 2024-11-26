261b63fcca3bc9949aa6c2ab7fe0e50e0f25a8a0
Ashen Thilakarathna <134621843+Ashein00@users.noreply.github.com>
2024-11-27 00:58:53 +0530

    Changed the objects for lighting and light added!

--- main.cpp
+++ main.cpp
@@ -121,26 +121,40 @@ static GLubyte quadIndices[6][4] =
     {0, 1, 2, 3}, //bottom
     {4, 5, 6, 7}, //top
     {5, 1, 2, 6}, //front
-    {0, 4, 7, 3}, //back is clockwise
+    {0, 4, 7, 3}, // back is clockwise
     {2, 3, 7, 6}, //right
     {1, 5, 4, 0}  //left is clockwise
 };
 
-static GLfloat colors[6][3] =
+
+static void getNormal3p
+(GLfloat x1, GLfloat y1, GLfloat z1, GLfloat x2, GLfloat y2, GLfloat z2, GLfloat x3, GLfloat y3, GLfloat z3)
 {
-    {0.4, 0.1, 0.0}, //bottom
-    {0.6, 0.0, 0.7}, //top
-    {0.0, 1.0, 0.0},
-    {0.0, 1.0, 1.0},
-    {0.8, 0.0, 0.0},
-    {0.3, 0.6, 0.7}
-};
+    GLfloat Ux, Uy, Uz, Vx, Vy, Vz, Nx, Ny, Nz;
+
+    Ux = x2 - x1;
+    Uy = y2 - y1;
+    Uz = z2 - z1;
+
+    Vx = x3 - x1;
+    Vy = y3 - y1;
+    Vz = z3 - z1;
+
+    Nx = Uy * Vz - Uz * Vy;
+    Ny = Uz * Vx - Ux * Vz;
+    Nz = Ux * Vy - Uy * Vx;
+
+    glNormal3f(Nx, Ny, Nz);
+}
 
 void drawCube()
 {
     glBegin(GL_QUADS);
     for (GLint i = 0; i < 6; i++)
     {
+        getNormal3p(v_cube[quadIndices[i][0]][0], v_cube[quadIndices[i][0]][1], v_cube[quadIndices[i][0]][2],
+            v_cube[quadIndices[i][1]][0], v_cube[quadIndices[i][1]][1], v_cube[quadIndices[i][1]][2],
+            v_cube[quadIndices[i][2]][0], v_cube[quadIndices[i][2]][1], v_cube[quadIndices[i][2]][2]);
         glVertex3fv(&v_cube[quadIndices[i][0]][0]);
         glVertex3fv(&v_cube[quadIndices[i][1]][0]);
         glVertex3fv(&v_cube[quadIndices[i][2]][0]);
@@ -149,118 +163,152 @@ void drawCube()
     glEnd();
 }
 
-void base() {
+void drawCube1(GLfloat difX, GLfloat difY, GLfloat difZ, GLfloat ambX = 0, GLfloat ambY = 0, GLfloat ambZ = 0, GLfloat shine = 50)
+{
+    GLfloat no_mat[] = { 0.0, 0.0, 0.0, 1.0 };
+    GLfloat mat_ambient[] = { ambX, ambY, ambZ, 1.0 };
+    GLfloat mat_diffuse[] = { difX, difY, difZ, 1.0 };
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { shine };
 
-    // right wall
-    glColor3f(1, 0.8, 0.5);
-    glPushMatrix();
-    glTranslatef(-1.5, -1, .5);
-    glScalef(5, 2, 0.1);
-    drawCube();
-    glPopMatrix();
+    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glMaterialfv(GL_FRONT, GL_EMISSION, no_mat);
 
-    // left wall
-    glColor3f(1, 0.8, 0.7);
-    glPushMatrix();
-    glTranslatef(-4.5, -1, 0);
-    glScalef(1, 2, 5);
-    drawCube();
-    glPopMatrix();
+    glBegin(GL_QUADS);
 
-    //ceiling
-    glColor3f(1.0, 0.9, 0.8);
-    glPushMatrix();
-    //glScalef(5, 0.1, 7);
-    glTranslatef(-2, 5.1, 0);
-    glScalef(5, 0.1, 7);
-    drawCube();
-    glPopMatrix();
+    for (GLint i = 0; i < 6; i++)
+    {
+        getNormal3p(v_cube[quadIndices[i][0]][0], v_cube[quadIndices[i][0]][1], v_cube[quadIndices[i][0]][2],
+            v_cube[quadIndices[i][1]][0], v_cube[quadIndices[i][1]][1], v_cube[quadIndices[i][1]][2],
+            v_cube[quadIndices[i][2]][0], v_cube[quadIndices[i][2]][1], v_cube[quadIndices[i][2]][2]);
+        glVertex3fv(&v_cube[quadIndices[i][0]][0]);
+        glVertex3fv(&v_cube[quadIndices[i][1]][0]);
+        glVertex3fv(&v_cube[quadIndices[i][2]][0]);
+        glVertex3fv(&v_cube[quadIndices[i][3]][0]);
+    }
+    glEnd();
+}
 
+void room()
+{
     // carpet
-    glColor3f(0.4, 0.1, 0.0);
+    //glColor3f(0.4, 0.1, 0.0);
     glPushMatrix();
     //glScalef(5, 0.1, 7);
     glTranslatef(3, -0.2, 7);
     glScalef(1.3, 0.01, 1.7);
-    drawCube();
+    drawCube1(0.4, 0.1, 0.0, 0.20, 0.05, 0.0);
+    glPopMatrix();
+
+    // right wall
+    //glColor3f(1, 0.8, 0.5);
+    glPushMatrix();
+    glTranslatef(-1.5, -1, .5);
+    glScalef(5, 2, 0.1);
+    //drawCube1(1, 0.8, 0.5,  0.5,0.4,0.25);
+    drawCube1(1, 0.8, 0.7, 0.5, 0.4, 0.35);
+    glPopMatrix();
+
+    // left wall
+    //glColor3f(1, 0.8, 0.7);
+    glPushMatrix();
+    glTranslatef(-4.5, -1, 0);
+    glScalef(1, 2, 5);
+    drawCube1(1, 0.8, 0.7, 0.5, 0.4, 0.35);
+    glPopMatrix();
+
+    // wall besides the right wall
+    //glColor3f(1, 0.8, 0.7);
+    glPushMatrix();
+    glTranslatef(8, -1, 0);
+    glScalef(0.2, 2, 5);
+    drawCube1(1, 0.8, 0.7, 0.5, 0.4, 0.35);
+    glPopMatrix();
+
+    //ceiling
+     //glColor3f(1.0, 0.9, 0.8);
+    glPushMatrix();
+    glTranslatef(-2, 5.1, 0);
+    glScalef(5, 0.1, 7);
+    drawCube1(1.0, 0.9, 0.8, 0.5, 0.45, 0.4);
+    glPopMatrix();
+
+    // floor
+    glPushMatrix();
+    glScalef(5, 0.1, 7);
+    glTranslatef(-1, -5, 0); //-1,-5,.5
+    //glScalef(5, 0.1, 7);
+    drawCube1(0.5, 0.1, 0.0, 0.25, 0.05, 0);
     glPopMatrix();
 }
 
-void window() {
 
+void window()
+{
+    //Window ********************************************
     //window white open
-    glColor3f(1.0, 1.0, 1.0);
     glPushMatrix();
     glTranslatef(-0.9, 1, 8.9); //0.5,1,9.6
-    //glRotatef(22, 0,0,1);
     glScalef(0.0001, .6, .3);
-    drawCube();
+    drawCube1(1.0, 1.0, 1.0, 0.05, 0.05, 0.05);
     glPopMatrix();
 
-    //window side corner
-    glColor3f(0.8, 0.6, 0.4);
+    //window right side corner
     glPushMatrix();
     glTranslatef(-0.9, 1, 8.9);
-    //glRotatef(22, 0,0,1);
     glScalef(0.04, 0.6, 0.0001);
-    drawCube();
+    drawCube1(0.8, 0.6, 0.4, 0.4, 0.3, 0.2);
     glPopMatrix();
 
     //window left side corner
-    glColor3f(0.8, 0.6, 0.4);
     glPushMatrix();
     glTranslatef(-0.9, 1, 9.8);
     glScalef(0.04, 0.6, 0.0001);
-    drawCube();
+    drawCube1(0.8, 0.6, 0.4, 0.4, 0.3, 0.2);
     glPopMatrix();
 
     //window upper side corner
-    glColor3f(0.7, 0.6, 0.5);
     glPushMatrix();
     glTranslatef(-0.7, 2.7, 8.9);
-    //glRotatef(22, 0,0,1);
     glScalef(0.0001, 0.05, 0.4);
-    drawCube();
+    drawCube1(0.7, 0.6, 0.5, 0.35, 0.3, 0.25);
     glPopMatrix();
 
 
     //window lower side corner
-    glColor3f(0.7, 0.6, 0.5);
     glPushMatrix();
     glTranslatef(-0.8, 1.02, 8.9);
-    //glRotatef(22, 0,0,1);
     glScalef(0.0001, 0.02, 0.34);
-    drawCube();
+    drawCube1(0.7, 0.6, 0.5, 0.35, 0.3, 0.25);
     glPopMatrix();
 
     //window vertical bar 1
-    glColor3f(0.0, 0.0, 0.0);
     glPushMatrix();
     glTranslatef(-0.87, 2.1, 8.9);
     //glRotatef(22, 0,0,1);
     glScalef(0.0001, 0.02, 0.3);
-    drawCube();
+    drawCube1(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5);
     glPopMatrix();
 
     //window vertical bar 2
-    glColor3f(0.0, 0.0, 0.0);
     glPushMatrix();
     glTranslatef(-0.87, 1.6, 8.9);
     //glRotatef(22, 0,0,1);
     glScalef(0.0001, 0.02, 0.3);
-    drawCube();
+    drawCube1(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5);
     glPopMatrix();
 
     //window horizontal bar
-    glColor3f(0.0, 0.0, 0.0);
     glPushMatrix();
     glTranslatef(-0.87, 1, 9.3);
     //glRotatef(22, 0,0,1);
     glScalef(0.0001, 0.6, 0.02);
-    drawCube();
+    drawCube1(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 5);
     glPopMatrix();
 }
-
 void fan()
 {
     glPushMatrix();
@@ -272,7 +320,7 @@ void fan()
     glTranslatef(0.1, 0, 0.09);
     glScalef(0.01, 0.4, 0.01);
     //glTranslatef(-1.5,-1.5,-1.5);
-    drawCube();
+    drawCube1(0.2, 0.1, 0.1, 0.2, 0.1, 0.1, 100); // Stand with shininess
     glPopMatrix();
 
     //fan cube
@@ -280,7 +328,7 @@ void fan()
     glPushMatrix();
     glScalef(0.1, 0.05, 0.1);
     glTranslatef(-1.5, -1.5, -1.5);
-    drawCube();
+    drawCube1(0.5, 0.2, 0.2, 0.5, 0.2, 0.2, 100); // Fan cube with shininess
     glPopMatrix();
 
     glPushMatrix();
@@ -292,7 +340,7 @@ void fan()
     //glRotatef(a, 0,1,0);
     glScalef(0.5, 0.01, 0.1);
     glTranslatef(-1.5, -1.5, -1.5);
-    drawCube();
+    drawCube1(0.8, 0.6, 0.4, 0.8, 0.6, 0.4, 100); // Blade 1 with shininess
     glPopMatrix();
 
     //blade 2 purple
@@ -301,7 +349,9 @@ void fan()
     //glRotatef(a, 0,1,0);
     glScalef(0.1, 0.01, 0.5);
     glTranslatef(-1.5, -1.5, -1.5);
-    drawCube();
+    drawCube1(0.8, 0.6, 0.4, 0.8, 0.6, 0.4, 100); // Blade 2 with shininess
+    glPopMatrix();
+
     glPopMatrix();
 
     glPopMatrix();
@@ -459,27 +509,25 @@ void lampLight() {
 }
 
 
-void display(void) {
+void display(void)
+{
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(60, windowWidth / windowHeight, 1, 100);
+    gluPerspective(60, 1, 1, 100);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(eyeX, eyeY, eyeZ, refX, refY, refZ, 0, 1, 0);
+    gluLookAt(eyeX, eyeY, eyeZ, refX, refY, refZ, 0, 1, 0); //7,2,15, 0,0,0, 0,1,0
 
     glEnable(GL_LIGHTING);
     lightOne();
     lightTwo();
     lampLight();
-    //lamp();
-
-    base();
-    window();
+    room();
     fan();
-
+    
     lightBulb1();
     lightBulb2();
     //lightBulb3();
@@ -716,10 +764,70 @@ void animate()
 
 }
 
+void fullScreen(int w, int h)
+{
+    //Prevent a divide by zero, when window is too short;you cant make a window of zero width.
+    if (h == 0)
+        h = 1;
+    float ratio = (GLfloat)w / (GLfloat)h;         //Calculate aspect ratio of the window
+
+    //Set the perspective coordinate system
+    glMatrixMode(GL_PROJECTION);                   //Use the Projection Matrix
+    glLoadIdentity();                              //Reset Matrix
+
+    glViewport(0, 0, w, h);                        //Set the viewport to be the entire window
+    gluPerspective(60, ratio, 1, 500);             //Set the correct perspective.
+    //glFrustum(-2.5,2.5,-2.5,2.5, ratio, 200);
+    glMatrixMode(GL_MODELVIEW);                    //Get Back to the Modelview
+}
+
 
 int main(int argc, char** argv)
 {
     glutInit(&argc, argv);
+
+    std::cout << "To move Eye point:" << std::endl;
+    std::cout << "w: up" << std::endl;
+    std::cout << "s: down" << std::endl;
+    std::cout << "a: left" << std::endl;
+    std::cout << "d: right" << std::endl;
+    std::cout << "i: zoom in" << std::endl;
+    std::cout << "o: zoom out" << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "To move Camera point:" << std::endl;
+    std::cout << "j: up" << std::endl;
+    std::cout << "n: down" << std::endl;
+    std::cout << "b: left" << std::endl;
+    std::cout << "m: right" << std::endl;
+    std::cout << "l: move nearer" << std::endl;
+    std::cout << "k: move far" << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "Press q to move to default position" << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "f: to turn on/off fan     " << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "For lighting:      " << std::endl;
+    std::cout << "Light source 1 [the light on the right on the screen      " << std::endl;
+    std::cout << "1: to turn on/off light one     " << std::endl;
+    std::cout << "4: to turn on/off ambient light one     " << std::endl;
+    std::cout << "5: to turn on/off diffusion light one     " << std::endl;
+    std::cout << "6: to turn on/off specular light one      " << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "Light source 2 [the light on the left on the screen " << std::endl;
+    std::cout << "2: to turn on/off light two     " << std::endl;
+    std::cout << "7: to turn on/off ambient light two     " << std::endl;
+    std::cout << "8: to turn on/off diffusion light two     " << std::endl;
+    std::cout << "9: to turn on/off specular light two      " << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "Lamp light (spot light)" << std::endl;
+    std::cout << "3: to turn on/off lamp     " << std::endl;
+    std::cout << "e: to turn on/off ambient lamp light    " << std::endl;
+    std::cout << "r: to turn on/off diffusion lamp light      " << std::endl;
+    std::cout << "t: to turn on/off specular lamp light      " << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "____________________" << std::endl;
+    std::cout << "      " << std::endl;
+    std::cout << "      " << std::endl;
 
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
@@ -729,11 +837,11 @@ int main(int argc, char** argv)
 
     glShadeModel(GL_SMOOTH);
     glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_NORMALIZE);
+    glEnable(GL_NORMALIZE);
+
+    glutReshapeFunc(fullScreen);
     glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
     glutKeyboardFunc(myKeyboardFunc);
-    //glutSpecialFunc(myKeyboardFunc);
     glutIdleFunc(animate);
     glutMainLoop();
 
